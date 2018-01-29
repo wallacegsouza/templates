864f3fbae42e34da70f913f1c6f6ba39d5091aa9
Wallace Gomes de Souza <wallacegomesdesouza@gmail.com>
2018-01-29 13:04:16 -0200

    ajuste no dockerfile

--- templates/spring-boot-web-tsuru/docker/start.sh
+++ templates/spring-boot-web-tsuru/docker/start.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 ./wait-for-it.sh database:5432 -t 15
-java -Djava.security.egd=file:/dev/./urandom -jar app.jar
+/home/ubuntu/.sdkman/candidates/java/current/bin/java -Djava.security.egd=file:/dev/./urandom -jar app.jar
