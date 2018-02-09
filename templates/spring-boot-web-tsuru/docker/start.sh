e21ae1051f67900708c3b05106c63265a8f13ac8
Jhonys Camacho <jhonyscamacho@campos.rj.gov.br>
2018-02-09 15:10:20 -0200

    Fix problem. Java command needed to change beacuse of Java image change.]

--- templates/spring-boot-web-tsuru/docker/start.sh
+++ templates/spring-boot-web-tsuru/docker/start.sh
@@ -1,3 +1,8 @@
 #! /bin/bash
 ./wait-for-it.sh database:5432 -t 15
-/home/ubuntu/.sdkman/candidates/java/current/bin/java -Djava.security.egd=file:/dev/./urandom -jar app.jar
+
+# Java command for jhonyscamacho/java-8u152-zulu - this image is temporarily not being used
+#/home/ubuntu/.sdkman/candidates/java/current/bin/java -Djava.security.egd=file:/dev/./urandom -jar app.jar
+
+# Java command for jhonyscamacho/java8
+java -Djava.security.egd=file:/dev/./urandom -jar app.jar
